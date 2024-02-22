ce5512efe84d9e61bdc9b134c900a04892adf9d5
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2024-02-22 08:33:48 +0100

    code for task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,5 +77,19 @@ def profile() -> str:
     return jsonify({"email": f"{user.email}"}), 200
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """Gets a user's password reset token"""
+
+    email = request.form.get("email")
+
+    try:
+        token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": f"{email}", "reset_token": f"{token}"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
