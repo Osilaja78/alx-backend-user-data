1f2abec9d15154cb70420ce28e553a3487b99357
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2024-02-22 06:37:10 +0100

    fixed sqlalchemy import error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from typing import Union
 import logging
 
@@ -64,3 +65,9 @@ class DB:
             raise InvalidRequestError()
 
         return user
+    
+    # def update_user(self, ) -> User:
+    #     """Updates user details in database"""
+
+    #     try:
+
