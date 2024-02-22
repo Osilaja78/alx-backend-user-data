35cb9300a17f60ce26a3c1cb9273c15800fa7e5f
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2024-02-22 06:30:14 +0100

    code for task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from typing import Union
 import logging
 
 from user import Base, User
@@ -48,4 +50,16 @@ class DB:
             print(f"Exception: {e}")
             self._session.rollback()
             raise e
+        return user
+
+    def find_user_by(self, **kwargs: Union[str, int]) -> User:
+        """Finds a user from arbitrary keyword arguments"""
+
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
+
         return user
